0bd1c7be1250bfd27215640168b80415cdd290ec
Nicolás Dos Santos <disney2santos@gmail.com>
2024-05-22 19:59:58 -0300

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,21 @@ import '../Styles.css';
 
 function CustomNavbar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="w-100">
-      <Container>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
         <Navbar.Brand as={Link} to="/">Lubren</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav className="me-auto justify-content-end">
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/about">Nosotros</Nav.Link>
             <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/services">All Services</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services">Servicios</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="/services/commercial">Commercial</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services/commercial">Comercial</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/services/industrial">Industrial</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
